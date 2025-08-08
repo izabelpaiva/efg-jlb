5e6c2043a2001e45869765f7b23993ba4ab6b7a3
Izabel Paiva <165713248+izabelpaiva@users.noreply.github.com>
2025-08-08 16:11:40 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+cidade = input('Digite sua cidade: ')
+mensagem = f'Bem-vindo(a) {nome}, sua idade é {idade} anos e você é de {cidade}!'
+
+print(mensagem)
