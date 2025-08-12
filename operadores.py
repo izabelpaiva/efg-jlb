1becbfbb6c78000518b66b2bfa6500765ff0af34
Izabel Paiva <165713248+izabelpaiva@users.noreply.github.com>
2025-08-12 16:35:47 -0300

    12-08

---
+++ operadores.py
@@ -0,0 +1,21 @@
+# Operadores Aritméicos:
+# + adição 2+3 = 5
+# - subtração 7-5 = 2
+# * multiplicação 4*3 = 12
+# / divisão 10/2 = 5
+# // divisão inteira 10//3 = 3
+# % módulo(resto da divisão) 10 % 3 = 1
+# ** exponenciação 2**3 = 8
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+print(f'A muliplicação de {n1} e {n2} é {n1*n2}')
+
+print(f'A divisão de {n1} e {n2} é {n1/n2}')
+
+print(f'A divisão inteira de {n1} e {n2} é {n1//n2}')
+
+print(f'O módulo de {n1} e {n2} é {n1%n2}')
+
+print(f'A exponencição de {n1} e {n2} é {n1**n2}')
