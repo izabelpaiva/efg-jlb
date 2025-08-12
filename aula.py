23583676fe4773920c487d30d1d51441bdf5e7c1
Izabel Paiva <165713248+izabelpaiva@users.noreply.github.com>
2025-08-12 16:06:46 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,4 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número para somar ao primeiro: '))
+
+print(f'A soma de {n1} e {n2} é {n1+n2}')
---
+++ numeros.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um número: '))
+
+print(f'Antecessor {n1-1}, seu número {n1}, sucessor {n1+1}')
